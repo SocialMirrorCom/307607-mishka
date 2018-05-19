b802812bbb7f7023d48030023f4381ff5cd66d45
socialmirrorcom <esenkina@villa-art.ru>
2018-05-19 12:39:34 +0100

    скорректировала пробелы в gulpfile, удалила fill из svg, исправила id svg в html файлах, добавила pipe server

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var run = require("run-sequence");
 Команда gulp clean*/
 
 gulp.task("clean", function () {
- return del("build");
+  return del("build");
 });
 
 /* Задание для копирования файлов в папку build
@@ -28,67 +28,69 @@ gulp.task("clean", function () {
 Команда gulp copy*/
 
 gulp.task("copy", function () {
- return gulp.src([
- "source/fonts/**/*.{woff,woff2}",
- "source/img/**",
- "source/js/**"
- ], {
- base: "source"
- })
- .pipe(gulp.dest("build"));
- });
+  return gulp.src([
+  "source/fonts/**/*.{woff,woff2}",
+  "source/img/**",
+  "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
 
 /* Задание запускает препроцессор, собирает файл css, делает префиксы, миминизирует и снова созраняет в папку build
 Команда gulp style*/
 
 gulp.task("style", function () {
- gulp.src("source/sass/style.scss")
- .pipe(plumber())
- .pipe(sass())
- .pipe(postcss([
- autoprefixer()
- ]))
- .pipe(gulp.dest("build/css"))
- .pipe(minify())
- .pipe(rename("style.min.css"))
- .pipe(gulp.dest("build/css"));
- });
+  gulp.src("source/sass/style.scss")
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(gulp.dest("build/css"))
+  .pipe(minify())
+  .pipe(rename("style.min.css"))
+  .pipe(gulp.dest("build/css"))
+  .pipe(server.stream());
+});
 
 /* Задание делает спрайт из файлов, на которые мы указываем, в инлайновом виде, то есть можно вносить изменения через CSS
 Команда gulp sprite*/
 
 gulp.task("sprite", function () {
- return gulp.src("source/img/icon-*.svg")
- .pipe(svgstore({
- inlineSvg: true
- }))
- .pipe(rename("sprite.svg"))
- .pipe(gulp.dest("build/img"));
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
 });
 
 /* Запускаем posthtml c плагином include, чтобы автоматически вставить спрайт в html файлы
 Команда gulp html*/
 
 gulp.task("html", function () {
- return gulp.src("source/*.html")
- .pipe(posthtml([
- include()
- ]))
- .pipe(gulp.dest("build"));
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("build"))
+  .pipe(server.stream());
 });
 
 /* Запускаем последовательно все задания
 Команда npm run build*/
 
 gulp.task("build", function (done) {
- run(
- "clean",
- "copy",
- "style",
- "sprite",
- "html",
- done
- );
+  run(
+  "clean",
+  "copy",
+  "style",
+  "sprite",
+  "html",
+  done
+  );
 });
 
 /* Живой сервер разработки, отсеживает изменения в sass и html  файлах и если фиксирует их
